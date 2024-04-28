ce3700fd73ee35ded1ab9f87d988399e8913ed59
DaleMcBride21 <dlmcburrito@gmail.com>
2024-04-27 21:13:08 -0600

    update

---
+++ scripts/getdates.js
@@ -0,0 +1,17 @@
+
+const year = document.querySelector("#year");
+
+const today = new Date();
+
+currentyear.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+
+// code below if for latest modification
+
+let oLastModif = new Date(document.lastModified);
+let lastModified = document.getElementById("lastModified");
+
+// Format the date for better readability
+let formattedDate = `${oLastModif.getDate()}/${oLastModif.getMonth() + 1}/${oLastModif.getFullYear()} ${oLastModif.getHours()}:${oLastModif.getMinutes()}:${oLastModif.getSeconds()}`;
+
+lastModified.innerHTML = `<p>Last Modification: ${formattedDate}</p>`;
